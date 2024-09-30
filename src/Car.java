440b0bcf2e8db3e62ae3de0f96a2898242ab378b
Filip Jinneklint <filip.jinneklint@elev.ga.ntig.se>
2024-09-30 09:42:56 +0200

    Skibidi

--- src/Car.java
+++ src/Car.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Car {
+    float speed = 0;
+    int maxSpeed;
+    String regNumber = "";
+    String[] passangers = new String[4];
+    ArrayList<String> trunk = new ArrayList<>();
+
+    public Car(String regNumber, int maxSpeed) {
+        maxSpeed = maxSpeed;
+        regNumber = regNumber;
+    }
+
+    public void speedUp(int change) {
+
+
+            if (speed + change < maxSpeed){
+                speed += change;
+        }
+    else
+        {
+            System.out.println("ajdinaid");
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("My car!");
+
+        Car car1 = new Car("ABC123", 200);
+        Car car2 = new Car("WOW420", 400);
+
+        System.out.println("Car 1 has regnumber: " + car1.regNumber + "and max speed: " + car1.maxSpeed);
+
     }
 }
